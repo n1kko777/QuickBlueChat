ce40fd8d133a29d4c908a45cd6b579c7612bca35
Nikita <3734489@gmail.com>
2016-08-04 12:25:35 +0500

    qwe

--- app/src/main/java/com/n1kko777/quickbluechat/About.java
+++ app/src/main/java/com/n1kko777/quickbluechat/About.java
@@ -1,12 +1,15 @@
 package com.n1kko777.quickbluechat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
 
-public class About extends AppCompatActivity {
+public class About extends AppCompatActivity implements View.OnClickListener {
 
     private Button buyButton;
 
@@ -16,8 +19,16 @@ public class About extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         buyButton = (Button)findViewById(R.id.button2);
+        buyButton.setOnClickListener(this);
 
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=K8B4S76YPKGFW"));
+        startActivity(browserIntent);
+
+    }
 }
