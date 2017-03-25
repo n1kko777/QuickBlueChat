8104a6a8ecde3fbd582d940e56f171da1ff83ceb
n1kko777 <3734489@gmail.com>
2017-03-25 15:17:24 +0500

    Update Library

--- app/src/main/java/com/n1kko777/quickbluechat/lue.java
+++ app/src/main/java/com/n1kko777/quickbluechat/lue.java
@@ -1,5 +1,6 @@
 package com.n1kko777.quickbluechat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -7,7 +8,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +22,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class lue extends Activity {
@@ -31,6 +36,8 @@ public class lue extends Activity {
     private ArrayAdapter<String> pairedDevicesArrayAdapter;
     private ArrayAdapter<String> newDevicesArrayAdapter;
 
+    private static final int REQUEST_PERMISSIONS = 1;
+
 
 
     public static String DEVICE_ADDRESS = "deviceAddress";
@@ -42,15 +49,83 @@ public class lue extends Activity {
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_lue);
 
+        requestPermission();
+
         Toast.makeText(lue.this, getResources().getString(R.string.tost), Toast.LENGTH_LONG).show();
-
-
         setResult(Activity.RESULT_CANCELED);
 
         getWidgetReferences();
         bindEventHandler();
         initializeValues();
 
+    }
+
+    private void requestPermission() {
+        String cameraPermission = Manifest.permission.CAMERA;
+        String bluetoothPermission = Manifest.permission.BLUETOOTH;
+        String bluetoothAdminPermission = Manifest.permission.BLUETOOTH_ADMIN;
+        String readPhoneStatePermition = Manifest.permission.READ_PHONE_STATE;
+        String readPermission = Manifest.permission.READ_EXTERNAL_STORAGE;
+        String writePermission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        String internetPermission = Manifest.permission.INTERNET;
+        String vibratePermission = Manifest.permission.VIBRATE;
+
+        int hasbluetoothPermission = 0;
+        int hasreadPhoneStatePermition = 0;
+        int hasbluetoothAdminPermission = 0;
+        int hascameraPermission = 0;
+        int hasreadPermission  = 0;
+        int haswritePermission = 0;
+        int hasinternetPermission = 0;
+        int hasvibratePermission = 0;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            hasreadPhoneStatePermition = checkSelfPermission(readPhoneStatePermition);
+            hascameraPermission = checkSelfPermission(cameraPermission);
+            hasbluetoothPermission = checkSelfPermission(bluetoothPermission);
+            hasbluetoothAdminPermission = checkSelfPermission(bluetoothAdminPermission);
+            hasreadPermission = checkSelfPermission(readPermission);
+            haswritePermission = checkSelfPermission(writePermission);
+            hasinternetPermission = checkSelfPermission(internetPermission);
+            hasvibratePermission = checkSelfPermission(vibratePermission);
+
+        }
+        List<String> permissions = new ArrayList<String>();
+
+        if (hasbluetoothPermission != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(bluetoothPermission);
+        }
+        if (hasreadPhoneStatePermition != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(readPhoneStatePermition);
+        }
+        if (hasbluetoothAdminPermission != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(bluetoothAdminPermission);
+        }
+
+        if (hascameraPermission != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(cameraPermission);
+        }
+        if (hasreadPermission != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(readPermission);
+        }
+        if (hasinternetPermission != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(internetPermission);
+        }
+        if (haswritePermission != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(writePermission);
+        }
+        if (hasvibratePermission != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(vibratePermission);
+        }
+        if (!permissions.isEmpty()) {
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(params, REQUEST_PERMISSIONS);
+            }
+        } else {
+            // We already have permission, so handle as normal
+
+        }
     }
 
     private void getWidgetReferences() {
